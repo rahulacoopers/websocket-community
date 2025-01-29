3f8a4b99dccc071473e7034ee10bc6a593b6824e
Rahula Palu <rahula_dd@hotmail.com>
2025-01-29 12:34:27 -0300

    Revert - try medel endpoint

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
 });
 
 // Azure Web PubSub configuration
-const connectionString = "Endpoint=https://cca-d-hpc-euw-wps.webpubsub.azure.com;AccessKey=Ba23pVSo165XrFSY76n0Wex2DwpKXULxFrvAeaFq2cfs5degD0LLJQQJ99BAAC5RqLJXJ3w3AAAAAWPSPhuA;Version=1.0;";
+const connectionString = "Endpoint=https://comm-pubsub-socket.webpubsub.azure.com;AccessKey=xNKGQNuKF+kVU66EvdGCvl3ntdWsxF/21rPbXoWl7e8=;Version=1.0;";
 const hubName = "Hub";
 
 // Setup Azure Web PubSub integration
